233f5796432b49c42d3fb0679602e3c7d54f9493
Fernando Catuo <fercatuo@gmail.com>
2024-11-27 22:46:32 -0600

    Configuracion de modelos, migraciones, factories, index y layout

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'image' => $this->faker->imageUrl(1280, 720),
+            'description' => $this->faker->text(800),
+
+            //Campos de la Relación
+
+            'user_id' => rand(1,5),
+            'category_id' => rand(1,3),
+
+        ];
+    }
+}
